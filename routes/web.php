9ff1b3664bc7ba00391a155be421d7bd7dac372d
Farna Chilashvili <farnnn.ff@gmail.com>
2024-07-14 21:53:15 +0400

    Create Job model and use it.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
 
 Route::get('/', function () {
     return view('home');
@@ -17,46 +17,13 @@ Route::get('/contact', function () {
 
 Route::get('/jobs', function () {
     return view('jobs', [
-        'jobs' => [
-            [
-                'id' => '1',
-                'title' => 'director',
-                'salary' => '$44000'
-            ],
-            [
-                'id' => '2',
-                'title' => 'programmer',
-                'salary' => '$144000'
-            ],
-            [
-                'id' => '3',
-                'title' => 'My assistant',
-                'salary' => '$14 000'
-            ],
-
-        ]
+        'jobs' => Job::all()
     ]);
 });
 
 Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        [
-            'id' => '1',
-            'title' => 'director',
-            'salary' => '$44000'
-        ],
-        [
-            'id' => '2',
-            'title' => 'programmer',
-            'salary' => '$144000'
-        ],
-        [
-            'id' => '3',
-            'title' => 'My assistant',
-            'salary' => '$14 000'
-        ]
-    ];
-    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+
+    $job = Job::find($id);
 
     return view('job', ['job' => $job]);
 });
