f20e3b6285546aac5ea0f586fd75b8d85ce42a69
Farna Chilashvili <farnnn.ff@gmail.com>
2024-07-15 18:23:19 +0400

    Add validation.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -29,10 +29,10 @@
                     <div class="hidden md:block">
                         <div class="ml-10 flex items-baseline space-x-4">
                             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                            <x-nav-link href="/" :active="request()->is('/')" >Home</x-nav-link>
-                            <x-nav-link href="/jobs" :active="request()->is('jobs')" >Jobs</x-nav-link>
-                            <x-nav-link href="/about" :active="request()->is('about')" >About</x-nav-link>
-                            <x-nav-link href="/contact" :active="request()->is('contact')" >Contact</x-nav-link>
+                            <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
+                            <x-nav-link href="/jobs" :active="request()->is('jobs')">Jobs</x-nav-link>
+                            <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
+                            <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
                         </div>
                     </div>
                 </div>
@@ -124,8 +124,9 @@
     </nav>
 
     <header class="bg-white shadow">
-        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 sm:flex sm:justify-between">
             <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $heading }}</h1>
+            <x-button href="/jobs/create">Create Job</x-button>
         </div>
     </header>
     <main>
