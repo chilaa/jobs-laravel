e3b9336433c51987e081e2ff8c86e63024bda42b
Farna Chilashvili <farnnn.ff@gmail.com>
2024-07-16 22:04:09 +0400

    Add policies and authorizations.

--- routes/web.php
+++ routes/web.php
@@ -9,12 +9,22 @@ Route::view('/', 'home');
 Route::view('/about', 'about');
 Route::view('/contact', 'contact');
 
-Route::resource('jobs', JobController::class);
+Route::controller(JobController::class)->group(function () {
+    Route::get('/jobs', 'index')->middleware('auth');
+    Route::get('/jobs/create', 'create');
+    Route::post('/jobs', 'store');
+    Route::get('/jobs/{job}', 'show');
+    Route::patch('/jobs/{job}', 'update');
+    Route::delete('/jobs/{job}', 'destroy');
+    Route::get('/jobs/{job}/edit', 'edit')
+        ->middleware('auth')
+        ->can('edit', 'job');
+});
 
 // Auth
 Route::get('/register', [RegisteredUserController::class, 'create']);
 Route::post('/register', [RegisteredUserController::class, 'store']);
 
-Route::get('/login', [SessionController::class, 'create']);
+Route::get('/login', [SessionController::class, 'create'])->name('login');
 Route::post('/login', [SessionController::class, 'store']);
 Route::post('/logout', [SessionController::class, 'destroy']);
