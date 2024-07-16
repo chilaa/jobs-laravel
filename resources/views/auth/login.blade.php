f2d59742655192d9ba63ca89aec16cac431b2be1
Farna Chilashvili <farnnn.ff@gmail.com>
2024-07-16 17:30:10 +0400

    Add field components, Add login and register form and controllers.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,31 +1,28 @@
 <x-layout>
     <x-slot:heading>
-        Create Job
+        Log In
     </x-slot:heading>
 
-    <form method="POST" action="/jobs">
+    <form method="POST" action="/login">
         @csrf
 
         <div class="space-y-12">
             <div class="border-b border-gray-900/10 pb-12">
-                <h2 class="text-base font-semibold leading-7 text-gray-900">Create a new job</h2>
-                <p class="mt-1 text-sm leading-6 text-gray-600">We just need some data about you.</p>
-
-                <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
 
                     <x-form-field class="sm:col-span-4">
-                        <x-form-label for="title">Title</x-form-label>
+                        <x-form-label for="email">Email</x-form-label>
                         <div class="mt-2">
-                            <x-form-input name="title" id="title" placeholder="CEO" required/>
-                            <x-form-error name="title"/>
+                            <x-form-input name="email" type="email" id="email" required/>
+                            <x-form-error name="email"/>
                         </div>
                     </x-form-field>
 
                     <x-form-field class="sm:col-span-4">
-                        <x-form-label for="salary">Salary</x-form-label>
+                        <x-form-label for="password">Password</x-form-label>
                         <div class="mt-2">
-                            <x-form-input name="salary" id="salary" placeholder="$50 000 USD" required/>
-                            <x-form-error name="salary"/>
+                            <x-form-input name="password" type="password" id="password" required/>
+                            <x-form-error name="password"/>
                         </div>
                     </x-form-field>
                 </div>
@@ -43,8 +40,8 @@
         </div>
 
         <div class="mt-6 flex items-center justify-end gap-x-6">
-            <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
-            <x-form-button>Save</x-form-button>
+            <a href="/" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
+            <x-form-button>Log In</x-form-button>
         </div>
     </form>
 </x-layout>
